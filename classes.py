28fc12ea3df2997696f206b0cf6fc2c363d89ca7
ShantanuKhan <shantanukhan@live.com>
2020-06-26 03:31:31 +0530

    commit fix

--- classes.py
+++ classes.py
@@ -4,4 +4,4 @@ circle1 = Circle(1,22,"red")
 circle2 = Circle(2,43,"pink")
 
 print("circumference of circle 1 is ",circle1.circumference())
-print("circumference of circle 2 is ",circle2.circumference())
+print("circumference of circle 2 is ",circle2.circumference()) 
